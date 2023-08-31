eba5833f407c20ea94dddf651237f7298c1e4917
JayshilShah <68106854+JayshilShah@users.noreply.github.com>
2023-08-31 23:19:32 +0530

    Functions created and made changes in the logic

--- ForIndia.cpp
+++ ForIndia.cpp
@@ -13,6 +13,97 @@ struct BankData
     double rateOfInflation;
 };
 
+const int maxDataSize = 100;
+struct BankData dataOfInvestment[maxDataSize];
+int index_of_array = 0;
+
+void fileRead(){
+    int dataSize = 0;
+    ifstream file("india_data.csv");
+    if (!file.is_open())
+    {
+        cerr << "Error occurred while opening the file." << endl;
+        return;
+    }
+    string line;
+    getline(file, line);
+    while (getline(file, line))
+    {
+        char *char_array = new char[line.length() + 1];
+        char_array[line.length()] = '\0';
+
+        for (int i = 0; i < line.length(); i++)
+        {
+            char_array[i] = line[i];
+        }
+        char *token = strtok(char_array, ",");
+        char *year = strtok(NULL, ",");
+        int yearInt;
+        sscanf(year, "%d", &yearInt);
+        dataOfInvestment[index_of_array].yearOfInvestment = yearInt;
+
+        char *interestRate = strtok(NULL, ",");
+        double interestRateDouble = stof(interestRate);
+        dataOfInvestment[index_of_array].rateOfInterest = interestRateDouble;
+
+        char *inflation = strtok(NULL, ",");
+        double inflationDouble = stof(inflation);
+        dataOfInvestment[index_of_array].rateOfInflation = inflationDouble;
+
+        index_of_array++;
+    }
+
+    file.close();
+}
+
+void calculateValueOfAmount(double investedAmount, int investedYear){
+    double amountInvested = investedAmount;
+    double result = 0;
+    if (investedYear < 1960 || investedYear >= 2023)
+    {
+        cout << "Enter valid year of investment" << endl;
+        return;
+    }
+    for (int i = 0; i < index_of_array; i++)
+    {
+        if (dataOfInvestment[i].yearOfInvestment >= investedYear)
+        {
+            investedAmount = investedAmount + (investedAmount * ((dataOfInvestment[i].rateOfInterest - dataOfInvestment[i].rateOfInflation) / 100.0));
+            result = investedAmount;
+        }
+    }
+    double decimalPart = result - int(result);
+    int paisa = int(decimalPart*100);
+    cout << "Money actually received: " << int(result) << " Rupees and " << paisa << " paisa" << endl;
+    float amomuntWithInterest;
+    float valueGained;
+    for (int i = 0; i < index_of_array; i++)
+    {
+        if (dataOfInvestment[i].yearOfInvestment == investedYear)
+        {
+            amomuntWithInterest = amountInvested + (amountInvested * ((dataOfInvestment[i].rateOfInterest - dataOfInvestment[i].rateOfInflation) / 100));
+            break;
+        }
+    }
+    double decimalPart1 = amomuntWithInterest - int(amomuntWithInterest);
+    int paisa1 = int(decimalPart1*100);
+    cout << "Value gained after the year of invesment: " << int(amomuntWithInterest) << " Rupees and " << paisa1 << " paisa" << endl;
+}
+
+int main()
+{    
+    fileRead();
+    double investment;
+    int investedYear;
+    cout << "Enter the amount of investment: ";
+    cin >> investment;
+    cout << "Enter the year of investment (after 1960): ";
+    cin >> investedYear;
+    calculateValueOfAmount(investment, investedYear);   
+
+    return 0;
+}
+
 
 // int main() {
 //     ifstream file;
@@ -25,87 +116,4 @@ struct BankData
 //     file.close();
 //     return 0;
 // }
-
-int main()
-{
-    const int maxDataSize = 100;
-    BankData data[maxDataSize];
-    int dataSize = 0;
-    ifstream file("india_data.csv");
-    if (!file.is_open())
-    {
-        cerr << "Error occurred while opening the file." << endl;
-        return 1;
-    }
-    string line;
-    getline(file, line);
-    int index = 0;
-    while (getline(file, line))
-    {
-        char *char_array = new char[line.length() + 1];
-        char_array[line.length()] = '\0';
-
-        for (int i = 0; i < line.length(); i++)
-        {
-            char_array[i] = line[i];
-        }
-        // Returns first token
-        char *token = strtok(char_array, ",");
-        char *year = strtok(NULL, ",");
-        int yearInt;
-        sscanf(year, "%d", &yearInt);
-        data[index].yearOfInvestment = yearInt;
-        char *interestRate = strtok(NULL, ",");
-        double interestRateDouble = stof(interestRate);
-        data[index].rateOfInterest = interestRateDouble;
-        char *inflation = strtok(NULL, ",");
-        double inflationDouble = stof(inflation);
-        data[index].rateOfInflation = inflationDouble;
-
-        index++;
-    }
-
-    file.close();
-
-    double investment, result = 0;
-    int yearInvested;
-
-    cout << "Enter the amount of investment: ";
-    cin >> investment;
-    cout << "Enter the year in which it was invested (after 1960): ";
-    cin >> yearInvested;
-
-    if (yearInvested < 1960)
-    {
-        cout << "Enter valid year of investment" << endl;
-        return -1;
-    }
-
-    int originalAmount = investment;
-
-    for (int i = 0; i < index; i++)
-    {
-        if (data[i].yearOfInvestment >= yearInvested)
-        {
-            investment = (investment * (1 + ((data[i].rateOfInterest - data[i].rateOfInflation) / 100.0)));
-            result = investment;
-        }
-    }
-
-    cout << "Money actually received: " << result << " INR" << endl;
-    float amomuntWithInterest;
-    float valueGained;
-    for (int i = 0; i < index; i++)
-    {
-        if (data[i].yearOfInvestment == yearInvested)
-        {
-            amomuntWithInterest = (originalAmount * (1 + data[i].rateOfInterest / 100));
-            valueGained = result - originalAmount;
-            break;
-        }
-    }
-
-    cout << "Value gained by the money after adding the interest of the investment year --> " << valueGained << " INR" << endl;
-
-    return 0;
-}
+            // valueGained = result - amomuntWithInterest;
